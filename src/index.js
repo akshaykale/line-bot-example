5d48dbf9f3b42cec844ab5659e0826c8e8708ae8
akshay.kale <akshay.kale@rakuten.com>
2017-03-21 18:33:32 +0900

    configs removed from index .js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ var config = require('config');
 var LINEBot = require('line-messaging');
 
 var bot = LINEBot.create({
-  channelID: config.get('line.channel_id'),
-  channelSecret: config.get('line.channel_secret'),
-  channelToken: config.get('line.channel_token')
+  channelID: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNELSECRET,
+  channelToken: process.env.CHANNEL_TOKEN
 });
 bot.webhook(process.env.HEROKU_URL);
 
