0199d896812cd7726f1332daed21560675160f6e
akshaykale <ark.akshaykale@gmail.com>
2017-03-21 19:48:04 +0900

    logger added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 var LINEBot = require('line-messaging');
+var logger = require("./logger.js");
 
 var bot = LINEBot.create({
   channelID: process.env.CHANNEL_ID,
@@ -10,18 +11,19 @@ bot.webhook('https://line-bot-simple.herokuapp.com/');
 
 
 // When we receive message
-bot.on(LINEBot.Events.MESSAGE, function(replyToken, message) {
-  
+bot.on(LINEBot.Events.MESSAGE, (replyToken, message) => {
+    logger.log("111");
     bot.replyTextMessage(replyToken, 'hello!').then(function(data) {
         return "200";
     }).catch(function(error) {
         return "404";
     });
-    
+    logger.log("222");
     return "error";
 
     }
 );
-
+logger.log("333");
 const port = process.env.PORT || 8080;
 bot.listen(port);
+logger.log("444");
